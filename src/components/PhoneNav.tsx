ae42a7d67fe1d30566fbb14767ad79a122af7502
Mohmd <mim.mohrechi@gmail.com>
2024-03-14 19:40:11 +0330

    added drop menu for phonenav

--- src/components/PhoneNav.tsx
+++ src/components/PhoneNav.tsx
@@ -1,24 +1,25 @@
 import Logo from "./ui/Logo";
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.js";
-const { theme } = resolveConfig(tailwindConfig);
-
+import { HamburgerIcon } from "./ui/Icons.js";
+import { useState } from "react";
+import PhoneNavDropMenu from "./ui/PhoneNavDropMenu.js";
 const PhoneNav = () => {
+    const [menu, setmenu] = useState(false);
+    const handleClick = () => {
+        setmenu((current) => !current);
+    };
     return (
-        <div
-            className="grid grid-cols-3 h-full text-black rounded-[0_0_1rem_1rem]"
-            style={{
-                background: `
-                url('src/assets/Alefba.svg') ,
-                linear-gradient(${theme.colors.grad1}, ${theme.colors.grad2})
-                `,
-                backgroundSize: "25% auto",
-            }}
-        >
-            <div></div>
-            <Logo />
-            <div></div>
-        </div>
+        <>
+            <div className="navBg grid relative z-20 grid-cols-3 h-full text-black rounded-[0_0_1rem_1rem]">
+                <div></div>
+                <Logo />
+                <div className="flex justify-end items-center pr-2 text-white">
+                    <button onClick={handleClick}>
+                        <HamburgerIcon />
+                    </button>
+                </div>
+            </div>
+            {menu && <PhoneNavDropMenu onClick={handleClick} />}
+        </>
     );
 };
 
