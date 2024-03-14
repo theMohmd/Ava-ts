78346813e0b1e4736e35e36102111a3dbc98dca3
Mohmd <mim.mohrechi@gmail.com>
2024-03-14 20:10:46 +0330

    commit

--- src/pages/Transcribe.tsx
+++ src/pages/Transcribe.tsx
@@ -1,8 +1,20 @@
-
 const Transcribe = () => {
-  return (
-    <div className="h-full grid items-center justify-center">Transcribe</div>
-  )
-}
+    return (
+        <div className="flex flex-col items-center h-full">
+            <div className="flex flex-col col-start-2 row-start-1 justify-center px-20 pt-5 text-center [direction:rtl]">
+                <h1 className="mb-2 font-bold text-cgreen text-[28px]">
+                    تبدیل گفتار به متن
+                </h1>
+                <p className="text-gray-400">
+                    آوا با استفاده از هزاران ساعت گفتار با صدای افراد مختلف،
+                    <br />
+                    زبان فارسی را یاد گرفته است و می‌تواند متن صحبت‌ها را
+                    بنویسد.
+                </p>
 
-export default Transcribe
+            </div>
+        </div>
+    );
+};
+
+export default Transcribe;
