1d534ef3b03d774ddabf0b0d13696bd758738e75
Mohmd <mim.mohrechi@gmail.com>
2024-03-19 02:24:38 +0330

    transcribe link finished with react query

---
+++ src/components/transcribe/link/useQueryLink.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+export const useQueryLink = (input: string, lang: "fa" | "en") =>
+    useQuery({
+        queryFn: () => {
+            return axios.post(
+                "https://harf.roshan-ai.ir/api/transcribe_files/",
+                {
+                    media_urls: [input],
+                    language: lang,
+                },
+                { headers: { Authorization: import.meta.env.VITE_API_KEY } }
+            );
+        },
+        queryKey: ["link"],
+        enabled: input !== "",
+    });
