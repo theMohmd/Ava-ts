d1eccbb7ddc7214ce1611efe91b1da586224b38b
Mohmd <mim.mohrechi@gmail.com>
2024-04-21 16:59:29 +0330

    fixed base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Ava-ts/',
+  base: '/Ava-ts',
   server:{
     proxy: {
       '/api': 'https://harf.roshan-ai.ir',
